944172e7ac466538c8ea1f7a06c4bcdd5ebdfe3a
deep-volue <138577784+deep-volue@users.noreply.github.com>
2023-10-04 12:13:20 +0200

    Update release_package.sh

--- releases/release_package.sh
+++ releases/release_package.sh
@@ -15,28 +15,28 @@ MODE="$1"
 PYPI_INDEX="$2"
 echo "PYPI_INDEX="$PYPI_INDEX
 
-if [ "$MODE" != "test" -a "$MODE" != "release" ]; then
+if [ "$MODE" != "test" && "$MODE" != "release" ]; then
   echo "Mode argument must be either 'test' or 'release'"
   exit 1
 fi
 
-if [ "$PYPI_INDEX" != "testpypi" -a "$PYPI_INDEX" != "pypi" ]; then
+if [ "$PYPI_INDEX" != "testpypi" && "$PYPI_INDEX" != "pypi" ]; then
   echo "PYPI_INDEX argument must be either 'testpypi' or 'pypi'"
   exit 1
 fi
 
-if [ "$VERSION" != "$PYVERSION" -a "$PYPI_INDEX" == "pypi" ]; then
+if [ "$VERSION" != "$PYVERSION" && "$PYPI_INDEX" == "pypi" ]; then
   echo "Version differs in VERSION and $SRCDIR/__init__.py ($VERSION != $PYVERSION)"
   exit 1
 fi
 
-if [ "$PYPI_INDEX" == "pypi" -a curl -f -H "Authorization: token $GITHUB_TOKEN" \
+if [ "$PYPI_INDEX" == "pypi" -&& curl -f -H "Authorization: token $GITHUB_TOKEN" \
         https://api.github.com/repos/volueinsight/$REPO/releases/tags/$RELEASE >/dev/null 2>&1 ]; then
   echo "Release $RELEASE already exists, update VERSION (and $SRCDIR/__init__.py)"
   exit 1
 fi
 
-if [ "$PYPI_INDEX" == "pypi" -a ! -r "./releases/relnote-$VERSION" ]; then
+if [ "$PYPI_INDEX" == "pypi" && ! -r "./releases/relnote-$VERSION" ]; then
   echo "Release note for $VERSION missing"
   exit 1
 fi

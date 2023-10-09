6d04f5e3fa5f1786585a108987016f6a2644b0e4
deep-volue <soumyadeep.ghosh@volue.com>
2023-10-09 12:38:18 +0200

    Updated theme

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,7 @@ pygments_style = 'sphinx'
 # documentation.
 #
 # html_theme_options = {}
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

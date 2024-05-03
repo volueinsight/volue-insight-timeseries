c4c885b256487812d4d680025f87167b5c0f8041
Jardar M. Bond <jardar.marshall.bond@volue.com>
2024-05-03 22:10:22 +0200

    Move dependencies used in examples to extras

--- setup.py
+++ setup.py
@@ -35,6 +35,12 @@ setup(
         'pytest-cov >= 2.5',
         'requests-mock >= 1.3',
     ],
+    extras_require={
+        'examples': [
+            'matplotlib',
+            'plotly',
+        ],
+    },
     version=version,
     description='Volue Insight API python library',
     long_description='This library is meant as a simple toolkit for working with data from https://api.volueinsight.com/ (or equivalent services).  Note that access is based on some sort of login credentials, this library is not all that useful unless you have a valid Volue Insight account.',

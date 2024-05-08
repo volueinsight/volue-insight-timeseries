e27c8b2abcaa46990515f2ce069f10ca86c2c8a8
jakubbelter <jakub.belter@volue.com>
2024-05-08 19:03:23 +0200

    tests: merge get data range into one parametrized test

--- unittest/test_get_data_range_curves.py
+++ unittest/test_get_data_range_curves.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 import pytest
 from zoneinfo import ZoneInfo
 from http import HTTPStatus
+from itertools import product
 from volue_insight_timeseries.util import Range, CurveException
 from volue_insight_timeseries.curves import InstanceCurve, TaggedCurve, TaggedInstanceCurve, TimeSeriesCurve
 
@@ -17,7 +18,7 @@ from helper.mock import RangeResponseMock, SessionMock
         (TimeSeriesCurve, [])
     ]
 )
-def test_curve_get_data_range__exception(dummy_curve_id, curve_class, method_args):
+def test_all_curve_types_get_data_range__exception(dummy_curve_id, curve_class, method_args):
     # arrange
     mock_session = SessionMock(resp=RangeResponseMock(HTTPStatus.INTERNAL_SERVER_ERROR, None, None), config_file=CONFIG_FILE)
     ts = curve_class(dummy_curve_id, metadata=None, session=mock_session)
@@ -40,7 +41,7 @@ def test_curve_get_data_range__exception(dummy_curve_id, curve_class, method_arg
         (InstanceCurve, ["2024-05-08", "1024-05-08"], HTTPStatus.NOT_FOUND)
     ]
 )
-def test_curve_get_data_range__empty(dummy_curve_id, curve_class, method_args, empty_status_code):
+def test_all_curve_types_get_data_range__empty(dummy_curve_id, curve_class, method_args, empty_status_code):
     # arrange
     mock_session = SessionMock(resp=RangeResponseMock(empty_status_code, None, None), config_file=CONFIG_FILE)
     ts = curve_class(dummy_curve_id, metadata=None, session=mock_session)
@@ -49,83 +50,60 @@ def test_curve_get_data_range__empty(dummy_curve_id, curve_class, method_args, e
     assert ts.get_data_range(*method_args) is None
 
 
-@pytest.mark.parametrize("range_begin,range_end", [("2024-05-08T00:00:00+02:00", "2024-05-11T22:00:00+02:00"), (None, None)])
-def test_instance_curve_get_data_range(dummy_curve_id, range_begin, range_end):
+# Define the classes and their URL bases
+curve_info = [
+    (InstanceCurve, "/api/instances/"),
+    (TaggedCurve, "/api/series/tagged/"),
+    (TaggedInstanceCurve, "/api/instances/tagged/"),
+    (TimeSeriesCurve, "/api/series/")
+]
+
+# Define the range parameters
+range_params = [
+    ("2024-05-08T00:00:00+02:00", "2024-05-11T22:00:00+02:00"),
+    (None, None)
+]
+
+@pytest.mark.parametrize(
+    "CurveCls, url_base, range_begin, range_end",
+    [
+        (curve_cls, url, start, end)
+        for (curve_cls, url), (start, end) in product(curve_info, range_params)
+    ]
+)
+def test_get_data_range(dummy_curve_id, CurveCls, url_base, range_begin, range_end):
     # arrange
-    param_from = "2024-05-03" 
-    param_to = "2024-05-12"
-    issue_date = "2024-04-01"
-    output_time_zone = "JST"
-    mock_session = SessionMock(resp=RangeResponseMock(HTTPStatus.OK, range_begin, range_end), config_file=CONFIG_FILE)
-    ts = InstanceCurve(dummy_curve_id, metadata=None, session=mock_session)
-
-    # act
-    range = ts.get_data_range(issue_date, param_from, param_to, output_time_zone)
-
-    # assert
-    tz = ZoneInfo("Asia/Tokyo")
-    assert mock_session.call_args == ["GET", "rtsp://test.host", f"/api/instances/{dummy_curve_id}/range?issue_date={issue_date}&from={param_from}&to={param_to}"]
-    assert range.begin == Range.parse_datetime(range_begin, tz)
-    assert range.end == Range.parse_datetime(range_end, tz)
-
-
-@pytest.mark.parametrize("range_begin,range_end", [("2024-05-08T00:00:00+02:00", "2024-05-11T22:00:00+02:00"), (None, None)])
-def test_tagged_curve_get_data_range(dummy_curve_id, range_begin, range_end):
-    # arrange
-    param_from = "2024-05-03" 
-    param_to = "2024-05-12"
-    tag = "tag-123"
-    output_time_zone = "JST"
-    mock_session = SessionMock(resp=RangeResponseMock(HTTPStatus.OK, range_begin, range_end), config_file=CONFIG_FILE)
-    ts = TaggedCurve(dummy_curve_id, metadata=None, session=mock_session)
-
-    # act
-    range = ts.get_data_range(tag, param_from, param_to, output_time_zone)
-
-    # assert
-    tz = ZoneInfo("Asia/Tokyo")
-    assert mock_session.call_args == ["GET", "rtsp://test.host", f"/api/series/tagged/{dummy_curve_id}/range?tag={tag}&from={param_from}&to={param_to}"]
-    assert range.begin == Range.parse_datetime(range_begin, tz)
-    assert range.end == Range.parse_datetime(range_end, tz)
-
-
-@pytest.mark.parametrize("range_begin,range_end", [("2024-05-08T00:00:00+02:00", "2024-05-11T22:00:00+02:00"), (None, None)])
-def test_tagged_instance_curve_get_data_range(dummy_curve_id, range_begin, range_end):
-    # arrange
-    tag = "tag-123"
     param_from = "2024-05-03" 
     param_to = "2024-05-12"
     output_time_zone = "JST"
     issue_date = "2024-04-01"
-
+    tag = "tag-123"
     mock_session = SessionMock(resp=RangeResponseMock(HTTPStatus.OK, range_begin, range_end), config_file=CONFIG_FILE)
-    ts = TaggedInstanceCurve(dummy_curve_id, metadata=None, session=mock_session)
-
+    ts = CurveCls(dummy_curve_id, metadata=None, session=mock_session)
+    
+    url_part = url_base + f"{dummy_curve_id}/range"
+    
     # act
-    range = ts.get_data_range(issue_date, tag, param_from, param_to, output_time_zone)
+    if CurveCls == InstanceCurve:
+        range = ts.get_data_range(issue_date, param_from, param_to, output_time_zone)
+    elif CurveCls == TaggedCurve:
+        range = ts.get_data_range(tag, param_from, param_to, output_time_zone)
+    elif CurveCls == TaggedInstanceCurve:
+        range = ts.get_data_range(issue_date, tag, param_from, param_to, output_time_zone)
+    else:
+        range = ts.get_data_range(param_from, param_to, output_time_zone)
 
     # assert
     tz = ZoneInfo("Asia/Tokyo")
-    assert mock_session.call_args == ["GET", "rtsp://test.host", f"/api/instances/tagged/{dummy_curve_id}/range?issue_date={issue_date}&tag={tag}&from={param_from}&to={param_to}"]
-    assert range.begin == Range.parse_datetime(range_begin, tz)
-    assert range.end == Range.parse_datetime(range_end, tz)
-
-
-@pytest.mark.parametrize("range_begin,range_end", [("2024-05-08T00:00:00+02:00", "2024-05-11T22:00:00+02:00"), (None, None)])
-def test_timeseries_curve_get_data_range(dummy_curve_id, range_begin, range_end):
-    # arrange
-    param_from = "2024-05-03" 
-    param_to = "2024-05-12"
-    output_time_zone = "JST"
-    mock_session = SessionMock(resp=RangeResponseMock(HTTPStatus.OK, range_begin, range_end), config_file=CONFIG_FILE)
-    ts = TimeSeriesCurve(dummy_curve_id, metadata=None, session=mock_session)
-
-    # act
-    range = ts.get_data_range(param_from, param_to, output_time_zone)
-
-    # assert
-    tz = ZoneInfo("Asia/Tokyo")
-    assert mock_session.call_args == ["GET", "rtsp://test.host", f"/api/series/{dummy_curve_id}/range?from={param_from}&to={param_to}"]
+    if CurveCls == InstanceCurve:
+        assert mock_session.call_args == ["GET", "rtsp://test.host", f"{url_part}?issue_date={issue_date}&from={param_from}&to={param_to}"]
+    elif CurveCls == TaggedCurve:
+        assert mock_session.call_args == ["GET", "rtsp://test.host", f"{url_part}?tag={tag}&from={param_from}&to={param_to}"]
+    elif CurveCls == TaggedInstanceCurve:
+        assert mock_session.call_args == ["GET", "rtsp://test.host", f"{url_part}?issue_date={issue_date}&tag={tag}&from={param_from}&to={param_to}"]
+    else:
+        assert mock_session.call_args == ["GET", "rtsp://test.host", f"{url_part}?from={param_from}&to={param_to}"]
+    
     assert range.begin == Range.parse_datetime(range_begin, tz)
     assert range.end == Range.parse_datetime(range_end, tz)
 
